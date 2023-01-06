7bc375f90ffe31417fc37ebc912c10f22c77bd33
Your name <oluwaborihamzat@gmail.com>
2023-01-06 12:01:08 +0100

    feat: tested query hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { Box, Text } from "@components/base";
+import { useGetCoinListQuery } from "@store/services/coin";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data, error, isLoading } = useGetCoinListQuery();
+  if (isLoading) {
+    return (
+      <Box>
+        {" "}
+        <Text>Loading</Text>
+      </Box>
+    );
+  }
+
   return (
     <>
       <Head>
@@ -19,7 +30,9 @@ export default function Home() {
       </Head>
       <main>
         <Box color="text" px="5" py="4" bg="bg" br="3">
-          <Text fs="lg" ta="center">Hello World</Text>
+          <Text fs="lg" ta="center">
+            {JSON.stringify(data, null, 2)}
+          </Text>
         </Box>
       </main>
     </>
