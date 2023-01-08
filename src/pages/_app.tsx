f97f41f122a2ad47e71d9dbbbbe599649430d9b5
Your name <oluwaborihamzat@gmail.com>
2023-01-08 08:16:17 +0100

    feat: added support for layouts in next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@store";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
   );
 }
+
