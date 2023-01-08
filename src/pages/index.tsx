c3c4fa3a6b0af72bca86aec38cd3fafcb3baf4d4
Your name <oluwaborihamzat@gmail.com>
2023-01-08 08:14:56 +0100

    feat: completed page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { Box, Text } from "@components/base";
-import { useGetCoinListQuery } from "@store/services/coin";
+import {
+  AllCoins,
+  MainLayout,
+  MarketLeaders,
+  News,
+  CenterLayout,
+} from "@components/inc";
+import { ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { data, error, isLoading } = useGetCoinListQuery();
-  if (isLoading) {
-    return (
-      <Box>
-        {" "}
-        <Text>Loading</Text>
-      </Box>
-    );
-  }
-
   return (
     <>
       <Head>
@@ -28,13 +25,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Box color="text" px="5" py="4" bg="bg" br="3">
-          <Text fs="lg" ta="center">
-            {JSON.stringify(data, null, 2)}
-          </Text>
-        </Box>
-      </main>
+      <CenterLayout main1={<MarketLeaders/>} main2={<AllCoins/>} aSide={<News/>} />
     </>
   );
 }
+
+Home.getLayout = function getLayout(page:ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
